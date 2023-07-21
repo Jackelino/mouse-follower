628359ffbdc55e9aec3ae72e3ee2408cc5a14094
JoseFernandez2020 <codigo.gratuito2019@gmail.com>
2023-07-21 10:31:31 -0600

    construction circle mouse

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
+import {useEffect, useState} from "react";
 function App() {
-
+    const [enable, setEnable] = useState(false);
+    useEffect(() => {
+        console.log('useEffect')
+    }, [enable]);
     return (
-        <>
-            <h2>
-                Mouse
-            </h2>
-        </>
+        <main>
+            <div className="circle-mouse"/>
+            <button onClick={() => setEnable(!enable)}>
+                {enable ? 'Desactivar' : 'Activar'} Seguir puntero
+            </button>
+        </main>
     )
 }
 
