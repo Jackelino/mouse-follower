c1fbba6164dc81b5b2e5124424e1df821f0ea8fc
JoseFernandez2020 <codigo.gratuito2019@gmail.com>
2023-07-21 18:33:40 -0600

    cal pointer move

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css';
 import {useEffect, useState} from "react";
+
 function App() {
     const [enable, setEnable] = useState(false);
+    const [position, setPosition] = useState({x: 0, y: 0});
+
     useEffect(() => {
-        console.log('useEffect')
+        const handleMove = (event) => {
+            const {clientX, clientY} = event;
+            setPosition({x: clientX, y: clientY});
+        }
+        if (enable) {
+            window.addEventListener('pointermove', handleMove)
+        }
+        //cleanup (return)
+        // -> cuando el componente se desmonta
+        // -> cuando cambia las dependencias. antes de ejecutar el efecto nuevo.
+        return () => {
+            window.removeEventListener('pointermove', handleMove)
+        }
     }, [enable]);
     return (
         <main>
-            <div className="circle-mouse"/>
+            <div className="circle-mouse" style={{
+                transform: `translate(${position.x}px, ${position.y}px)`
+            }}/>
             <button onClick={() => setEnable(!enable)}>
                 {enable ? 'Desactivar' : 'Activar'} Seguir puntero
             </button>
